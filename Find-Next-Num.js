2ff27717cefe5e43bbe85665ba6014acf09dd599
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-14 08:05:50 +0530

    Create Find-Next-Num.js

---
+++ Find-Next-Num.js
@@ -0,0 +1,19 @@
+
+/*
+Create a function that takes a
+number as an argument, increments the
+number by +1 and returns the result.
+*/
+
+function findNextNum(num){
+     // num +1
+    console.log(num+1)
+}
+
+findNextNum(5)
+findNextNum(2)
+findNextNum(10)
+findNextNum(0)
+findNextNum(-4)
+
+
