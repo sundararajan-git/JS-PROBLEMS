c5689f6d8d4e33b03f8e842a8bdb07799b289425
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-14 08:29:10 +0530

    Create Power-Calculator.js

---
+++ Power-Calculator.js
@@ -0,0 +1,19 @@
+
+/*
+Create a function that takes
+voltage and current and returns 
+the calculated power.
+*/
+
+function powerCal(V,I){
+     // power = V * I 
+     console.log(V*I)
+}
+
+powerCal(230,10)
+powerCal(430,50)
+powerCal(120,16)
+
+
+
+
