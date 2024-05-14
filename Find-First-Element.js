95698544e83a538171443a65ce8c9ebc40778e29
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-14 08:23:22 +0530

    Create Find-First-Element.js

---
+++ Find-First-Element.js
@@ -0,0 +1,21 @@
+
+/*
+Create a function that takes an array 
+containing only numbers and 
+return the first element.
+*/
+
+function getFrstEle(arr){
+     // using index
+     console.log(arr[0])
+    // using method
+    console.log(arr.shift())
+}
+
+getFrstEle([2,45,78,-54])
+getFrstEle([-87,67,23,5])
+getFrstEle([109,56,45])
+
+
+
+
