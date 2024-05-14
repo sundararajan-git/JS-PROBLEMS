c05ee2181203c74d208266653f44ccda5227ad4e
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-14 08:00:40 +0530

    Create Minutes-To-Seconds.js

---
+++ Minutes-To-Seconds.js
@@ -0,0 +1,16 @@
+
+/*
+Write a function that takes an
+integer minutes and converts it to seconds.
+*/
+
+function minutesToseconds(min){
+     // 1 minutess = 60 seconds 
+    console.log(min*60)
+}
+
+minutesToseconds(5)
+minutesToseconds(2)
+minutesToseconds(10)
+
+
