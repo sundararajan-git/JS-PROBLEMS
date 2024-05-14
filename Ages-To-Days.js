7c13ca486e46653fcc6fa3b3c51399f8e19dba42
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-14 08:11:44 +0530

    Create Ages-To-Days.js

---
+++ Ages-To-Days.js
@@ -0,0 +1,20 @@
+
+/*
+Create a function that takes 
+the age in years and returns the
+age in days.
+*/
+
+function ageToDays(age){
+     // it approximate not accurate same time leap year cross
+    // 1 year = 365 days
+    console.log(age * 365)
+}
+
+ageToDays(5)
+ageToDays(20)
+ageToDays(54)
+
+
+
+
