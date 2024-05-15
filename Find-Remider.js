476e4fc9e03e65b54cf404d53ee7865b43fe7f14
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-15 08:46:57 +0530

    Create Find-Remider.js

---
+++ Find-Remider.js
@@ -0,0 +1,23 @@
+
+/*
+There is a single operator in
+JavaScript, capable of providing 
+the remainder of a division operation. 
+Two numbers are passed as parameters. 
+The first parameter divided by the second 
+parameter will have a remainder, possibly zero. Return that value.
+*/
+
+function findRemider(a,b){
+     //  a%b is formula
+     console.log(a%b)
+}
+
+findRemider(1,3)
+findRemider(3,4)
+findRemider(-9,45)
+findRemider(5,5)
+
+
+
+
