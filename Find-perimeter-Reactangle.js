8fb7b6622a0fa7b5b1ec6ba61420716318085e86
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-15 08:54:38 +0530

    Create Find-perimeter-Reactangle.js

---
+++ Find-perimeter-Reactangle.js
@@ -0,0 +1,20 @@
+
+/*
+Create a function that takes 
+length and width and finds the 
+perimeter of a rectangle.
+*/
+
+function findperimeter(a,b){
+     //  perimeter = 2*(a+b)
+    // perimeter means total length
+     console.log(2*(a+b))
+}
+
+findperimeter(6,7)
+findperimeter(20,10)
+findperimeter(2,9)
+
+
+
+
