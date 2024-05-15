b7f0e70d269da860e6971b38eee1460733d487ba
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-15 08:28:33 +0530

    Create Hours-To-Seconds.js

---
+++ Hours-To-Seconds.js
@@ -0,0 +1,18 @@
+
+/*
+Write a function that 
+converts hours into seconds.
+*/
+
+function hoursToseconds(hours){
+     //  1 minutes = 60seconds ,  60 minutes = 3600
+     console.log(hours*3600)
+}
+
+hoursToseconds(2)
+hoursToseconds(3)
+hoursToseconds(5)
+
+
+
+
