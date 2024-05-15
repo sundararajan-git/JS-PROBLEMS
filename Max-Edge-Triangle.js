d30a6909ade44a079685203d34b5fa62a89beb44
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-15 08:40:49 +0530

    Create Max-Edge-Triangle.js

---
+++ Max-Edge-Triangle.js
@@ -0,0 +1,20 @@
+
+/*
+Create a function that finds the 
+maximum range of a triangle's 
+third edge, where the side lengths
+are all integers.
+*/
+
+function maxEdge(a,b){
+     //  formula = (side1 +side2) -1
+     console.log((a+b)-1)
+}
+
+maxEdge(8,10)
+maxEdge(5,7)
+maxEdge(9,2)
+
+
+
+
