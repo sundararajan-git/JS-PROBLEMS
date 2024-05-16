2198fc4ff82ee79fb90f7580602d9782258f20ea
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-16 08:32:27 +0530

    Create Sum-Of-Less.js

---
+++ Sum-Of-Less.js
@@ -0,0 +1,17 @@
+/*
+Given two numbers, return true
+if the sum of both numbers is less 
+than 100. Otherwise return false.
+*/
+
+function sumOfLess(a,b){
+    console.log((a+b)<100)
+}
+
+sumOfLess(70,20)
+sumOfLess(60,60)
+sumOfLess(2,67)
+
+
+
+
