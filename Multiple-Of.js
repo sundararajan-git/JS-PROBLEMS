cfa6cbe6d2a916afaa3f80e1bfdb462b77781394
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-17 09:20:48 +0530

    Create Multiple-Of.js

---
+++ Multiple-Of.js
@@ -0,0 +1,18 @@
+/*
+Create a function that takes an 
+integer and returns true if it's divisible by 100, 
+otherwise return false.
+*/
+
+function multipleof(num){
+  console.log((num%100 === 0) ? true : false )
+}
+
+multipleof(1)
+
+multipleof(1000) 
+
+multipleof(250) 
+
+
+
