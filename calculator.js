987e2be285bf32ab805e7452345913cb84f3efe7
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-17 09:13:16 +0530

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,20 @@
+/*
+Create a function that will handle 
+simple math expressions. 
+The input is an expression in the 
+form of a string.
+*/
+
+function calculator(exp){
+  console.log(eval(exp))
+}
+
+calculator("23+4")
+
+calculator("45-15") 
+
+calculator("13+2-5*2") 
+
+calculator("49/7*2-3") 
+
+
