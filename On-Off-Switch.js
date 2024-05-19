26b7ea0ecd398622c116ac910edde96d0a2416e0
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-19 08:43:17 +0530

    Create On-Off-Switch.js

---
+++ On-Off-Switch.js
@@ -0,0 +1,19 @@
+/*
+Create a function that returns 
+how many possible arrangements 
+can come from a certain number of 
+switches (on / off). In other words, 
+for a given number of switches, 
+how many different patterns of on 
+and off can we have?
+*/
+
+function onOff(num){
+console.log(2**num)
+}
+
+onOff(2)
+onOff(3)
+onOff(10)
+
+
