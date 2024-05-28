fcac767f54b0b5ad6608feb53f5c5f819ee26697
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-28 08:15:42 +0530

    Create Sum-Of-Two-Digits.js

---
+++ Sum-Of-Two-Digits.js
@@ -0,0 +1,32 @@
+
+/*
+
+For this challenge, you are supposed
+to find the sum of the digits of a two-digit number. 
+Sounds easy, right? But for this challenge, 
+I expect you to find the sum of the
+digits mathematically.
+
+Sure, you can convert the number
+into a string and then manipulate it so 
+it returns the sum of the digits, but the
+point of this challenge is to see if you know
+how to return the sum of the digits of a
+two-digit number mathematically.
+
+*/
+
+function sumOfTwoDigits(x){
+
+    console.log(Number(String(x)[0] ) + Number(String(x)[1]))
+
+}
+
+
+sumOfTwoDigits(45)
+
+sumOfTwoDigits(38)
+
+sumOfTwoDigits(67)
+
+
