e50de739fbfae8cb8c2ea83df5fcd2f9d7004c59
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-31 08:46:55 +0530

    Create Concat-Array.js

---
+++ Concat-Array.js
@@ -0,0 +1,18 @@
+/*
+Create a function to 
+concatenate two integer arrays.
+*/
+
+function concat(arr1,arr2){
+    console.log(arr1.concat(arr2))
+    // or
+     console.log([...arr1,...arr2].flat())
+}
+
+concat([1, 3, 5], [2, 6, 8]) // [1, 3, 5, 2, 6, 8]
+
+concat([7, 8], [10, 9, 1, 1, 2])  // [7, 8, 10, 9, 1, 1, 2]
+
+concat([4, 5, 1], [3, 3, 3, 3, 3]) // [4, 5, 1, 3, 3, 3, 3, 3]
+
+
