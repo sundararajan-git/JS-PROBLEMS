a63d3b74aa71dd33b7471ae8e5e31c78505e0513
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-31 08:29:59 +0530

    Create Reverse-Array.js

---
+++ Reverse-Array.js
@@ -0,0 +1,17 @@
+/*
+
+Write a function to reverse an array.
+
+*/
+
+function reverse(arr){
+     console.log(arr.reverse())
+}
+
+reverse([1, 2, 3, 4])  // [4, 3, 2, 1]
+
+reverse([9, 9, 2, 3, 4]) // [4, 3, 2, 9, 9]
+
+reverse([]) // []
+
+
