3af64ce911880a388fc292f3a4e62120e8e94f4d
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-05-31 08:07:03 +0530

    Create Pair-Management.js

---
+++ Pair-Management.js
@@ -0,0 +1,20 @@
+/*
+
+Given two arguments,
+return an array which contains these
+two arguments.
+
+*/
+
+function pairMan(...args){
+     console.log([...args])
+}
+
+
+pairMan(5,2,1)
+
+pairMan(1,2,3)
+
+pairMan(23,56,98,58,-67)
+
+
