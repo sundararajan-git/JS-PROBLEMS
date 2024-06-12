388adfbb090bbce209cf8276a8ec20193c6fa462
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-06-12 10:00:48 +0530

    Create Add-Index.js

---
+++ Add-Index.js
@@ -0,0 +1,20 @@
+/*
+Given an array of numbers, create a function which returns
+the same array but with each 
+element's index in the array added to itself. 
+This means you add 0 to the number at index 0,
+add 1 to the number at index 1, etc...
+*/
+
+function addIndexes(arr){
+    let out = arr.map((i,index)=>{
+      return  i+index
+    })
+    console.log(out)
+}
+
+addIndexes([0, 0, 0, 0, 0]) //  [0, 1, 2, 3, 4]
+
+addIndexes([1, 2, 3, 4, 5]) //  [1, 3, 5, 7, 9]
+
+addIndexes([5, 4, 3, 2, 1]) //  [5, 5, 5, 5, 5]
