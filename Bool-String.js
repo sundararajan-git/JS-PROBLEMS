be5de354e964937f32e69ce922f50dbf986512f6
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-06-21 08:34:08 +0530

    Create Bool-String.js

---
+++ Bool-String.js
@@ -0,0 +1,15 @@
+
+/*
+Create a function that takes a
+boolean variable flag and returns 
+it as a string.
+*/
+
+function  boolToString(bool){
+   console.log(String(bool))
+}
+
+
+boolToString(true) //  "true"
+
+boolToString(false) //  "false
