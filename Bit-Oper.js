8d91b9576be3f9a7ab59761b913e21a353352e06
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-06-24 09:40:03 +0530

    Create Bit-Oper.js

---
+++ Bit-Oper.js
@@ -0,0 +1,23 @@
+
+/*
+Write three functions to calculate the bitwise AND,
+bitwise OR and bitwise XOR of two numbers.
+*/
+
+function  bitwiseAND(x,y){
+    console.log(x&y)
+}
+
+bitwiseAND(7, 12) // 4
+
+function bitwiseOR(x,y) {
+    console.log(x | y)
+}
+
+bitwiseOR(7, 12) // 15
+
+function bitwiseXOR(x,y) {
+    console.log(x^y)
+}
+
+bitwiseXOR(7, 12) //  11
