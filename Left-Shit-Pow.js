0e0e206a9441e10d53b0266aa3cc86ec3aff0f92
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-06-25 08:11:16 +0530

    Create Left-Shit-Pow.js

---
+++ Left-Shit-Pow.js
@@ -0,0 +1,22 @@
+
+/*
+The left shift operation is similar to 
+multiplication by powers of two.
+*/
+
+function  shiftToLeft(x,y){
+    console.log(x<<y)
+}
+
+
+shiftToLeft(5, 2) // 20
+
+shiftToLeft(10, 3) //  80
+
+shiftToLeft(-32, 2) //  -128
+
+shiftToLeft(-6, 5) //  -192
+
+shiftToLeft(12, 4) // 192
+
+shiftToLeft(46, 6) //  2944
