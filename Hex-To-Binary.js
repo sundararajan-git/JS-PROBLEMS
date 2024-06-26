8136617381bfde070d98e84c6dda18941dc25a28
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-06-26 08:22:02 +0530

    Create Hex-To-Binary.js

---
+++ Hex-To-Binary.js
@@ -0,0 +1,15 @@
+
+/*
+Hex to Binary
+*/
+
+function  toBinary(n){
+    console.log(n.toString(2))
+}
+
+
+toBinary(0xFF) //  "11111111"
+
+toBinary(0xAA)  // "10101010"
+
+toBinary(0xFA) //  "11111010"
