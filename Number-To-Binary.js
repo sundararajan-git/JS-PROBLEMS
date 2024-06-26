ee4b4c06fd492ce58747dedc8231d49cba6ef7ea
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-06-26 08:18:41 +0530

    Create Number-To-Binary.js

---
+++ Number-To-Binary.js
@@ -0,0 +1,17 @@
+
+/*
+Convert a Number to Base-2
+*/
+
+function  binary(n){
+    console.log(n.toString(2))
+}
+
+binary(1) // "1"
+// 1*1 = 1
+
+binary(5) //  "101"
+// 1*1 + 1*4 = 5
+
+binary(10)  // "1010"
+// 1*2 + 1*8 = 10
