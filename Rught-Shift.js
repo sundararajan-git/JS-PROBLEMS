948cd83f764ff2225934ffc0625b2f8ea747208a
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2024-06-27 08:19:31 +0530

    Create Rught-Shift.js

---
+++ Rught-Shift.js
@@ -0,0 +1,22 @@
+
+/*
+The right shift operation is similar to
+floor division by powers of two.
+*/
+
+function  shiftToRight(x,y){
+    console.log(x >> y)
+}
+
+
+shiftToRight(80, 3) // 10
+
+shiftToRight(-24, 2) //  -6
+
+shiftToRight(-5, 1) // -3
+
+shiftToRight(4666, 6) // 72
+
+shiftToRight(3777, 6) //  59
+
+shiftToRight(-512, 10) //  -1
